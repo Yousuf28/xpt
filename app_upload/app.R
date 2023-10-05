d3910310f9553f934db4b232c77f4cec2d1a73c7
Yousuf Ali <yousuf.pharma@gmail.com>
2023-10-05 13:48:01 -0400

    xpt

--- app_upload/app.R
+++ app_upload/app.R
@@ -13,40 +13,42 @@ ui <- fluidPage(
     sidebarPanel(
 
       # Input: Select a file ----
-      fileInput("file1", "Choose CSV File",
-                multiple = FALSE,
-                accept = c("text/csv",
-                         "text/comma-separated-values,text/plain",
-                         ".csv")),
-
+      ## fileInput("file1", "Choose CSV File",
+      ##           multiple = FALSE,
+      ##           accept = c(".xpt","text/csv",
+      ##                    "text/comma-separated-values,text/plain",
+      ##                    ".csv")),
+shiny::fileInput('file1',label = 'Choose xpt file',
+                 multiple = FALSE,
+                 accept = ".xpt")
       # Horizontal line ----
-      tags$hr(),
+      ## tags$hr(),
 
-      # Input: Checkbox if file has header ----
-      checkboxInput("header", "Header", TRUE),
+      ## # Input: Checkbox if file has header ----
+      ## checkboxInput("header", "Header", TRUE),
 
-      # Input: Select separator ----
-      radioButtons("sep", "Separator",
-                   choices = c(Comma = ",",
-                               Semicolon = ";",
-                               Tab = "\t"),
-                   selected = ","),
+      ## # Input: Select separator ----
+      ## radioButtons("sep", "Separator",
+      ##              choices = c(Comma = ",",
+      ##                          Semicolon = ";",
+      ##                          Tab = "\t"),
+      ##              selected = ","),
 
-      # Input: Select quotes ----
-      radioButtons("quote", "Quote",
-                   choices = c(None = "",
-                               "Double Quote" = '\\"',
-                               "Single Quote" = "'"),
-                   selected = '\\"'),
+      ## # Input: Select quotes ----
+      ## radioButtons("quote", "Quote",
+      ##              choices = c(None = "",
+      ##                          "Double Quote" = '\\"',
+      ##                          "Single Quote" = "'"),
+      ##              selected = '\\"'),
 
-      # Horizontal line ----
-      tags$hr(),
+      ## # Horizontal line ----
+      ## tags$hr(),
 
-      # Input: Select number of rows to display ----
-      radioButtons("disp", "Display",
-                   choices = c(Head = "head",
-                               All = "all"),
-                   selected = "head")
+      ## # Input: Select number of rows to display ----
+      ## radioButtons("disp", "Display",
+      ##              choices = c(Head = "head",
+      ##                          All = "all"),
+      ##              selected = "head")
 
     ),
 
@@ -54,7 +56,8 @@ ui <- fluidPage(
     mainPanel(
 
       # Output: Data file ----
-      tableOutput("contents")
+      DT::DTOutput('contents')
+      ## tableOutput("contents")
 
     )
 
@@ -64,7 +67,8 @@ ui <- fluidPage(
 # Define server logic to read selected file ----
 server <- function(input, output) {
 
-  output$contents <- renderTable({
+  output$contents <- DT::renderDT({
+  ## output$contents <- renderTable({
 
     # input$file1 will be NULL initially. After the user selects
     # and uploads a file, head of that data file by default,
@@ -74,25 +78,10 @@ server <- function(input, output) {
 
     # when reading semicolon separated files,
     # having a comma separator causes `read.csv` to error
-    tryCatch(
-      {
-        df <- read.csv(input$file1$datapath,
-                 header = input$header,
-                 sep = input$sep,
-                 quote = input$quote)
-      },
-      error = function(e) {
-        # return a safeError if a parsing error occurs
-        stop(safeError(e))
-      }
-    )
+        df <- haven::read_xpt(input$file1$datapath)
+        df
 
-    if(input$disp == "head") {
-      return(head(df))
-    }
-    else {
-      return(df)
-    }
+
 
   })
 
