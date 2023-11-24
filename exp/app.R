88ee7a486b8d4f1706615641e71c54978bad8b49
Yousuf Ali <yousuf.pharma@gmail.com>
2023-11-24 13:25:19 -0500

    csv downlaod added

--- exp/app.R
+++ exp/app.R
@@ -26,48 +26,25 @@ ui <- shiny::fluidPage(
 
                                  ## shiny::tags$br(),
 
-                                 downloadButton('down_xpt', 'Download file')
+                                 downloadButton('down_xpt', 'Download file as xpt'),
+                                 shiny::tags$hr(style = "border-top: 1px dashed black"),
+                                 downloadButton('down_csv', 'Download file as csv')
+
                                ),
 
                shiny::mainPanel(
                         DT::DTOutput('contents')
                       )))
 
-     ##           shiny::fluidRow(
-     ## shiny::column(width = 2,offset = 3,
-     ##  shiny::fileInput('file1', "Choose xpt file",
-     ##                   accept = '.xpt')
-     ##        ),
-     ## shiny::column(width = 1,
-     ##  shiny::actionButton('add_row', label = "Add Row"),
-     ##  shiny::tags$hr(),
-
-     ##  shiny::actionButton('delete_row', label = "Delete Row"),
-
-     ##  shiny::tags$hr(),
-     ##  ## shiny::tags$br(),
-
-     ##  downloadButton('down_xpt', 'Download file')
-     ##               )
-     ## ## shiny::column(width = 1,
-     ## ##               ),
-     ## ## shiny::column(width = 1,
-     ## ##               ),
-     ##                  ),
-     ##  shiny::tags$hr(),
-     ##  DT::DTOutput('contents')
-## )
 
 # Define server logic to read selected file ----
 server <- function(input, output) {
   v <- shiny::reactiveVal()
   shiny::observeEvent(input$file1,{
-   shiny::req(input$file1)
-  tab <- haven::read_xpt(input$file1$datapath)
-  ## tab <- SASxport::read.xport(input$file1$datapath)
-  v(tab)
-
-  
+    shiny::req(input$file1)
+    tab <- haven::read_xpt(input$file1$datapath)
+    ## tab <- SASxport::read.xport(input$file1$datapath)
+    v(tab)
   })
  # render tabel
   output$contents <- DT::renderDT({
@@ -75,16 +52,16 @@ server <- function(input, output) {
     DT::datatable(v(),
                   ## selection = 'single',
                   ## options = list(dom = 't'),
-              editable = T)
+                  editable = T)
   })
 # add row , by default add last row to the begining of table
-shiny::observeEvent(input$add_row,{
-  df <- v()
-  nr <- df[nrow(df), ]
-  t <- rbind(nr,df)
-v(t)
-})
-## selected row will be deleted. if no row seletecd, no row deleted
+  shiny::observeEvent(input$add_row,{
+    df <- v()
+    nr <- df[nrow(df), ]
+    t <- rbind(nr,df)
+    v(t)
+  })
+  ## selected row will be deleted. if no row seletecd, no row deleted
   shiny::observeEvent(input$delete_row, {
     t  <- v()
     if (!is.null(input$contents_rows_selected)) {
@@ -102,20 +79,35 @@ v(t)
     t[edit_row,edit_col] <- edit_value
     v(t)
 })
-# download file
+# download file as xpt
   output$down_xpt <- shiny::downloadHandler(
-    filename = function() {
-     file_name <- strsplit(input$file1$name, '.xpt')[[1]]
-      print(file_name)
-      paste0(file_name, ".xpt")
+                              filename = function() {
+                                file_name <- strsplit(input$file1$name, '.xpt')[[1]]
+                                ## print(file_name)
+                                paste0(file_name, ".xpt")
 
-    },
-    content = function(file) {
-      df <- v()
-     ## SASxport::write.xport(df, file=file)
-      haven::write_xpt(df, file)
-    }
-  )
+                              },
+                              content = function(file) {
+                                df <- v()
+                                ## SASxport::write.xport(df, file=file)
+                                haven::write_xpt(df, file)
+                              }
+                            )
+
+ # download csv
+  output$down_csv <- shiny::downloadHandler(
+                              filename = function() {
+                                file_name <- strsplit(input$file1$name, '.xpt')[[1]]
+                                print(file_name)
+                                paste0(file_name, ".csv")
+
+                              },
+                              content = function(file) {
+                                df <- v()
+                                write.csv(df, file=file, quote= FALSE, row.names=FALSE)
+                              }
+                            )
+
 }
-# Create Shiny app ----
+  # Create Shiny app ----
 shiny::shinyApp(ui, server)
