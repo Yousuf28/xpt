6c7b65f628e5e503101ffd892bb482e77dff367c
Yousuf Ali <yousuf.pharma@gmail.com>
2023-11-24 12:29:14 -0500

    python file deleted

--- test.py
+++
@@ -1,9 +0,0 @@
-import pyreadstat
-import pandas as pd
-#df=pd.read_sas('bw.xpt',encoding='utf-8')
-df, meta= pyreadstat.read_xport('bw.xpt')
-
-print(df.head())
-print(meta.column_labels)
-#print(dir(df))
-#print(df.info)
