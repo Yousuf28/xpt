a1aed94b240f35e7254765039da48789cdf673db
Yousuf Ali <yousuf.pharma@gmail.com>
2023-11-24 12:23:05 -0500

    row add delete

---
+++ README.md
@@ -0,0 +1,30 @@
+## xptView
+xptView shiny app can be use to
+- Read xpt file and view the table
+- edit specific value in the table
+- add rows
+- delete rows
+- download after editing the table
+
+## workflow
+
+read xpt file from your system  
+![example](documents/example.jpg)  
+
+### add row
+to add row, hit add row button.  
+this will add a row at the top of the table. By default, it copies the last row
+from the table. You need to edit by clicking on the cell.  
+### delete row
+one or more rows can be deleted at once.
+first select row/rows you want to delete in the table. Then 
+hit delete row button.  
+![delete](documents/delete.jpg)  
+
+
+
+### download
+after editing, table can be downloaded as xpt file.
+
+
+
--- exp/app.R
+++ exp/app.R
@@ -1,5 +1,4 @@
 ## library(shiny)
-## library(SASxport)
 # library(DT)
 # library(haven)
 
@@ -10,109 +9,113 @@ downloadButton <- function(...) {
 }
 # Define UI for data upload app ----
 ui <- shiny::fluidPage(
-      
-      shiny::fileInput('file1', "Choose xpt file",
-                       accept = '.xpt'),
+               shiny::titlePanel('xptView'),
 
-      shiny::tags$hr(),
-      downloadButton('down_xpt', 'Download file'),
-      shiny::tags$hr(),
-      shiny::tags$hr(),
-      DT::DTOutput('contents')
+               shiny::sidebarLayout(
+                        shiny::sidebarPanel(width = 2,
 
-    # )
+                                 shiny::fileInput('file1', "Choose xpt file",
+                                                  accept = '.xpt'),
+                                 shiny::tags$hr(style = "border-top: 1px dashed black"),
+                                 ## shiny::tags$br(),
+                                 shiny::actionButton('add_row', label = "Add Row"),
+                                 shiny::tags$hr(style = "border-top: 1px dashed black"),
 
-  
-)
+                                 shiny::actionButton('delete_row', label = "Delete Row"),
+                                 shiny::tags$hr(style = "border-top: 1px dashed black"),
+
+                                 ## shiny::tags$br(),
+
+                                 downloadButton('down_xpt', 'Download file')
+                               ),
+
+               shiny::mainPanel(
+                        DT::DTOutput('contents')
+                      )))
+
+     ##           shiny::fluidRow(
+     ## shiny::column(width = 2,offset = 3,
+     ##  shiny::fileInput('file1', "Choose xpt file",
+     ##                   accept = '.xpt')
+     ##        ),
+     ## shiny::column(width = 1,
+     ##  shiny::actionButton('add_row', label = "Add Row"),
+     ##  shiny::tags$hr(),
+
+     ##  shiny::actionButton('delete_row', label = "Delete Row"),
+
+     ##  shiny::tags$hr(),
+     ##  ## shiny::tags$br(),
+
+     ##  downloadButton('down_xpt', 'Download file')
+     ##               )
+     ## ## shiny::column(width = 1,
+     ## ##               ),
+     ## ## shiny::column(width = 1,
+     ## ##               ),
+     ##                  ),
+     ##  shiny::tags$hr(),
+     ##  DT::DTOutput('contents')
+## )
 
 # Define server logic to read selected file ----
 server <- function(input, output) {
-  v <- shiny::reactiveValues()
+  v <- shiny::reactiveVal()
   shiny::observeEvent(input$file1,{
    shiny::req(input$file1)
-    # print(input$file1$name)
   tab <- haven::read_xpt(input$file1$datapath)
   ## tab <- SASxport::read.xport(input$file1$datapath)
-  ## tab <- Hmisc::sasxport.get(input$file1$datapath, lowernames = F)
-  v$tab <- tab  
+  v(tab)
+
   
   })
-  
-# df <-  shiny::eventReactive(input$file1, {
-#    req(input$file1)
-#   tab <- haven::read_xpt(input$file1$datapath)
-#   tab
-#  })
+ # render tabel
   output$contents <- DT::renderDT({
     shiny::req(input$file1)
-        df <- DT::datatable(v$tab,selection = 'none', editable = TRUE)
-        df
-
+    DT::datatable(v(),
+                  ## selection = 'single',
+                  ## options = list(dom = 't'),
+              editable = T)
   })
-  # proxy = DT::dataTableProxy('contents')
-  # 
-  # observeEvent(input$contents_cell_edit, {
-  #   info = input$contents_cell_edit
-  #   str(info)
-  #   i = info$row
-  #   j = info$col
-  #   v = info$value
-  #   df[i, j] <<- DT::coerceValue(v, df[i, j])
-  #   replaceData(proxy, df, resetPaging = FALSE)  # important
-  # })
-  # 
-  
-  # proxy5 = dataTableProxy('contents')
+# add row , by default add last row to the begining of table
+shiny::observeEvent(input$add_row,{
+  df <- v()
+  nr <- df[nrow(df), ]
+  t <- rbind(nr,df)
+v(t)
+})
+## selected row will be deleted. if no row seletecd, no row deleted
+  shiny::observeEvent(input$delete_row, {
+    t  <- v()
+    if (!is.null(input$contents_rows_selected)) {
+      t <- t[-as.numeric(input$contents_rows_selected),]
+    }
+    v(t)
+  })
+# update cell value
   shiny::observeEvent(input$contents_cell_edit, {
-    # info = input$contents_cell_edit
-    v$tab <<- DT::editData(v$tab,input[['contents_cell_edit']], 'contents')
-    print(names(v$tab))
-    ## print(head(v$tab))
-    # str(info)  # check what info looks like (a data frame of 3 columns)
-    # print(info)
-    # t <- df()
-    # print(head(t))
-    # df <<- editData(t, info)
-    # replaceData(proxy5, df, resetPaging = FALSE)  # important
-    # the above steps can be merged into a single editData() call; see examples below
-  })
-  
+    info <- input$contents_cell_edit
+    edit_row <-  info$row
+    edit_col <-  info$col
+    edit_value <-  info$value
+    t  <-  v()
+    t[edit_row,edit_col] <- edit_value
+    v(t)
+})
+# download file
   output$down_xpt <- shiny::downloadHandler(
     filename = function() {
-      # Sys.sleep(2)
-      # file_name <- basename(input$file1$datapath)
      file_name <- strsplit(input$file1$name, '.xpt')[[1]]
       print(file_name)
-
       paste0(file_name, ".xpt")
-      ## paste0('bw.xpt')
 
     },
     content = function(file) {
-      df <- v$tab
-      ## print(head(df))
-      ## print(names(df))
-      ## Sys.sleep(2)
-     ## write.csv(df, file, row.names = FALSE)
+      df <- v()
      ## SASxport::write.xport(df, file=file)
-    ## print(file)
-## kk <- sub(tools::file_path_sans_ext(basename(file)), 'hello',file)
-      ## temp_dir <- tempdir()
-      ## print(temp_dir)
-      ## path <- fs::path(temp_dir, 'dm.xpt')
-
-      ## fs::file_move(file, path)
-
-      ## fs::file_copy(file, kk)
-
-     ## xportr::xportr_write(df, path = path )
       haven::write_xpt(df, file)
-      ## fs::file_copy(path, file)
     }
   )
-  
-
 }
-
 # Create Shiny app ----
 shiny::shinyApp(ui, server)
