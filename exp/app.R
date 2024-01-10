8854e6170289b182c09d223d8bc4eb3cf0af4efb
Yousuf Ali <yousuf.pharma@gmail.com>
2024-01-09 21:17:24 -0500

    xlsx support added

--- exp/app.R
+++ exp/app.R
@@ -1,6 +1,18 @@
-## library(shiny)
+##  This code is for creating shiny app that can read and write xpt file.
+## xpt file also can be exported as xlsx (Excel) and csv file
+
+## -----------------------------------------------------------------------------
+##   Date                     Programmer
+## ----------   --------------------------------------------------------------
+##   Oct-05-2023    Md Yousuf Ali (md.ali@fda.hhs.gov)
+
+# required R packages
+# library(shiny)
 # library(DT)
 # library(haven)
+# library(writexl)
+# library(readxl)
+# library(fs)
 
 source('create_xlsx.R')
 downloadButton <- function(...) {
@@ -10,34 +22,35 @@ downloadButton <- function(...) {
 }
 # Define UI for data upload app ----
 ui <- shiny::fluidPage(
-               shiny::titlePanel('xptView'),
+  shiny::titlePanel('xptView'),
 
-               shiny::sidebarLayout(
-                        shiny::sidebarPanel(width = 2,
+  shiny::sidebarLayout(
+    shiny::sidebarPanel(width = 2,
 
-                                 shiny::fileInput('file1', "Choose xpt/xlsx file",
-                                                  accept = c('.xpt', '.xlsx')),
-                                 shiny::tags$hr(style = "border-top: 1px dashed black"),
-                                 ## shiny::tags$br(),
-                                 shiny::actionButton('add_row', label = "Add Row"),
-                                 shiny::tags$hr(style = "border-top: 1px dashed black"),
+    # file upload
+      shiny::fileInput('file1', 'Choose xpt/xlsx file',
+        accept = c('.xpt', '.xlsx')),
+      shiny::tags$hr(style = 'border-top: 1px dashed black'),
+      # add row
+      shiny::actionButton('add_row', label = 'Add Row'),
+      shiny::tags$hr(style = 'border-top: 1px dashed black'),
+      # delete row
+      shiny::actionButton('delete_row', label = 'Delete Row'),
+      shiny::tags$hr(style = 'border-top: 1px dashed black'),
+      # download as xpt
+      downloadButton('down_xpt', 'Download file as xpt'),
+      shiny::tags$hr(style = 'border-top: 1px dashed black'),
+      # download as xlsx
+      downloadButton('down_xls', 'Download as excel file'),
+      shiny::tags$hr(style = 'border-top: 1px dashed black'),
+      # download as csv
+      downloadButton('down_csv', 'Download file as csv')
 
-                                 shiny::actionButton('delete_row', label = "Delete Row"),
-                                 shiny::tags$hr(style = "border-top: 1px dashed black"),
-
-                                 ## shiny::tags$br(),
-
-                                 downloadButton('down_xpt', 'Download file as xpt'),
-                                 shiny::tags$hr(style = "border-top: 1px dashed black"),
-                                 downloadButton('down_xls', 'Download as excel file'),
-                                 shiny::tags$hr(style = "border-top: 1px dashed black"),
-                                 downloadButton('down_csv', 'Download file as csv')
-
-                               ),
-
-               shiny::mainPanel(
-                        DT::DTOutput('contents')
-                      )))
+    ),
+# table
+    shiny::mainPanel(
+      DT::DTOutput('contents')
+    )))
 
 
 # Define server logic to read selected file ----
@@ -45,30 +58,24 @@ server <- function(input, output) {
   v <- shiny::reactiveVal()
   shiny::observeEvent(input$file1,{
     shiny::req(input$file1)
-    print(input$file1$datapath)
     ext <- basename(input$file1$datapath)
-
     if(grepl('xpt', ext, ignore.case = T)){
-
-    tab <- haven::read_xpt(input$file1$datapath)
-    ## tab <- SASxport::read.xport(input$file1$datapath)
-    v(tab)
-
+      tab <- haven::read_xpt(input$file1$datapath)
+      v(tab)
     } else{
       tab <- get_xlsx(input$file1$datapath)
-
       v(tab)
     }
   })
- # render tabel
+  # render tabel
   output$contents <- DT::renderDT({
     shiny::req(input$file1)
     DT::datatable(v(),
-                  ## selection = 'single',
-                  ## options = list(dom = 't'),
-                  editable = T)
+      ## selection = 'single',
+      ## options = list(dom = 't'),
+      editable = T)
   })
-# add row , by default add last row to the begining of table
+  # add row , by default add last row to the begining of table
   shiny::observeEvent(input$add_row,{
     df <- v()
     nr <- df[nrow(df), ]
@@ -83,7 +90,7 @@ server <- function(input, output) {
     }
     v(t)
   })
-# update cell value
+  # update cell value
   shiny::observeEvent(input$contents_cell_edit, {
     info <- input$contents_cell_edit
     edit_row <-  info$row
@@ -93,71 +100,71 @@ server <- function(input, output) {
     t[edit_row,edit_col] <- edit_value
     v(t)
 })
-# download file as xpt
+# get file name so it can be use in download section
+  get_file_name <- shiny::eventReactive(input$file1, {
+
+    ext <- basename(input$file1$datapath)
+    if(grepl('xpt', ext, ignore.case= TRUE)) {
+      file_name <- strsplit(input$file1$name, '.xpt')[[1]]
+    } else {
+      file_name <- strsplit(input$file1$name, '.xlsx')[[1]]
+    }
+    ## filename <- paste0(file_name, ".xpt")
+    file_name
+
+
+  })
+  # download file as xpt
   output$down_xpt <- shiny::downloadHandler(
-                              filename = function() {
-                                file_name <- strsplit(input$file1$name, '.xpt')[[1]]
-                                
-                                 # print(file_name)
-                                paste0(file_name, ".xpt")
-                              
-
+    filename = function() {
+      file_name <- get_file_name()
+      file_name <- paste0(file_name, '.xpt')
                               },
-                              content = function(file) {
-                                df <- v()
-                                domain <- strsplit(input$file1$name, '.xpt')[[1]]
-                               temp_dir <- tempdir()
-                               ## print(temp_dir)
-                               path <- fs::path(temp_dir, paste0(toupper(domain), '.xpt'))
-                                ## SASxport::write.xport(df, file=file)
-                                
-                                haven::write_xpt(data = df, path=path, version = 5)
-                                fs::file_copy(path, file)
-                              }
-                            )
+    content = function(file) {
+      df <- v()
+      domain <- get_file_name()
+      temp_dir <- tempdir()
+      path <- fs::path(temp_dir, paste0(domain, '.xpt'))
+      haven::write_xpt(data = df, path=path, version = 5)
+      fs::file_copy(path, file)
+    }
+  )
 
- # download csv
+  # download csv
   output$down_csv <- shiny::downloadHandler(
-                              filename = function() {
-                                file_name <- strsplit(input$file1$name, '.xpt')[[1]]
-                                ## print(file_name)
-                                paste0(file_name, ".csv")
+    filename = function() {
+      file_name <- get_file_name()
+      file_name <- paste0(file_name, '.csv')
+    },
+    content = function(file) {
+      df <- v()
+      write.csv(df, file=file, quote= FALSE, row.names=FALSE)
+    }
+  )
 
-                              },
-                              content = function(file) {
-                                df <- v()
-                                write.csv(df, file=file, quote= FALSE, row.names=FALSE)
-                              }
-                            )
+  # download xlsx (xcel file)
+  output$down_xls  <- shiny::downloadHandler(
+    filename = function() {
+      file_name <- get_file_name()
+      paste0(file_name, '.xlsx')
+    },
+    content = function(file) {
+      df <- v()
+      var_meta <- create_xlsx(df)
+      col <- dim(df)[2]
+      var_n <- dim(df)[1]
+      dataset_name <- toupper(get_file_name())
+      df_meta <- data.frame(Dataset=c(dataset_name),
+        Label=c(NA), Variable=c(col),
+        Records=c(var_n))
 
-# download xlsx (xcel file)
-output$down_xls  <- shiny::downloadHandler(
+     out <- list('Dataset Metadata'=df_meta, 'Variable Metadata'= var_meta)
+      out[[dataset_name]] <- df
+      print(names(out))
+      writexl::write_xlsx(out, path = file)
 
-                             filename = function() {
-                               file_name <- strsplit(input$file1$name, '.xpt')[[1]]
-                               paste0(file_name, ".xlsx")
-
-                             },
-                             content = function(file) {
-                               df <- v()
-                               var_meta <- create_xlsx(df)
-                               col <- dim(df)[2]
-                               var_n <- dim(df)[1]
-                               file_name <- strsplit(input$file1$name, '.xpt')[[1]]
-                               dataset_name <- toupper(file_name)
-                               df_meta <- data.frame(Dataset=c(dataset_name),
-                                                 Label=c(NA), Variable=c(col),
-                                                 Records=c(var_n))
-                               writexl::write_xlsx(list(`Dataset Metadata`=df_meta,
-                                                        `Variable Metadata`= var_meta,
-                                                        BW=df),
-                                                   path= file)
-
-                             }
-
-                           )
-
-
+    }
+  )
 }
-  # Create Shiny app ----
+# Create Shiny app ----
 shiny::shinyApp(ui, server)
--- exp/create_xlsx.R
+++ exp/create_xlsx.R
@@ -1,12 +1,11 @@
+##  This code is for creating xlsx file
 
-##  This code is for creating write xlsx file
+## -----------------------------------------------------------------------------
+##   Date                     Programmer
+## ----------   --------------------------------------------------------------
+##   Dec-28-2023    Md Yousuf Ali (md.ali@fda.hhs.gov)
 
-## History:
-##   -----------------------------------------------------------------------------
-##   Date         Programmer            Note
-## ----------   --------------------  ------------------------------------------
-##   2023-12-28    Yousuf Ali        Initial version
-
+# this function get attribute of dataframe and return attribute as dataframe
 create_xlsx <- function(df) {
 
   df_label <- sapply(df, attr, 'label')
@@ -17,7 +16,7 @@ create_xlsx <- function(df) {
   vars <- c()
   # format like $2
   form_sas <- c()
- fm <- df_format
+  fm <- df_format
   for (i in 1:length(fm)){
     vars <- c(vars, names(fm[i]))
     if( is.null(fm[[i]])) {
@@ -30,24 +29,24 @@ create_xlsx <- function(df) {
   }
 
 # get digit from $2
-leng <- c()
+  leng <- c()
   # get $ or NA
-form <- c()
-for(i in 1:length(form_sas)) {
+  form <- c()
+  for(i in 1:length(form_sas)) {
 
   if(!is.na(form_sas[i])) {
 
     dg <- unlist(strsplit(form_sas[i], '\\$'))[2]
     dol <- unlist(strsplit(form_sas[i], ''))[1]
-   leng <- c(leng,dg)
+    leng <- c(leng,dg)
     form <- c(form, dol)
   } else {
     leng <- c(leng, '8')
     form <- c(form, NA)
 
   }
-}
-leng <- as.integer(leng)
+  }
+  leng <- as.integer(leng)
 
   type_var <- unlist(lapply(df,class))
 
@@ -65,16 +64,21 @@ leng <- as.integer(leng)
 ## kd <- create_xlsx(df)
 ## kd
 
+## this function read edited/downloaded xlsx file,
+## it get metadata from variable metadate sheet
+## and add the meta data to domain dataframe
+
 get_xlsx <- function(file) {
 
-v_meta <- readxl::read_xlsx(file, sheet = 'Variable Metadata')
-vm <- gsub('character', 'text', v_meta$Type)
-df_xl <- readxl::read_xlsx(file, sheet = 3, col_types = vm)
+  v_meta <- readxl::read_xlsx(file, sheet = 'Variable Metadata')
+  vm <- gsub('character', 'text', v_meta$Type)
+  df_xl <- readxl::read_xlsx(file, sheet = 3, col_types = vm)
+  df_xl[] <- lapply(df_xl, function(x) ifelse(is.character(x) & is.na(x), "", x))
 for (i in 1:length(v_meta$Variable)) {
   attr(df_xl[[i]], 'label') <- v_meta$Label[[i]]
   if(!is.na(v_meta$Format[i])) {
-  formSAS <- paste0(v_meta$Length[i],v_meta$Format[i])
-  attr(df_xl[[i]], 'format.sas') <- formSAS
+    formSAS <- paste0(v_meta$Format[i], v_meta$Length[i])
+    attr(df_xl[[i]], 'format.sas') <- formSAS
   ## print(formSAS)
   } else {
     attr(df_xl[[i]], 'format.sas') <- NULL
@@ -82,7 +86,7 @@ for (i in 1:length(v_meta$Variable)) {
 }
   return(df_xl)
 }
-
-#example
+ #example
+## d <- get_xlsx('')
 ## df_xl <- get_xlsx('bw_func.xlsx')
 ## str(df_xl)
